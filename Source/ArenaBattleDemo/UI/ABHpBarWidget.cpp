700fb8346e1b16512ab9720a771c7b40721a4193
Sangmin Yun <dbstkdalsa@naver.com>
2025-04-30 11:03:52 +0900

    HP 바에 텍스트 표시 추가

--- Source/ArenaBattleDemo/UI/ABHpBarWidget.cpp
+++ Source/ArenaBattleDemo/UI/ABHpBarWidget.cpp
@@ -3,7 +3,9 @@
 
 #include "UI/ABHpBarWidget.h"
 #include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
 #include "Interface/ABCharacterWidgetInterface.h"
+#include "Character/ABCharacterBase.h"
 
 UABHpBarWidget::UABHpBarWidget(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
@@ -14,14 +16,46 @@ UABHpBarWidget::UABHpBarWidget(const FObjectInitializer& ObjectInitializer)
 
 void UABHpBarWidget::UpdateHpBar(float NewCurrentHp)
 {
+	// 현재 HP 값 업데이트.
+	CurrentHp = NewCurrentHp;
+	
 	// MaxHp 값이 제대로 설정됐는지 확인.
 	ensure(MaxHp > 0.0f);
 
 	// 프로그레스 바 위젯 업데이트.
 	if (HpProgressBar)
 	{
-		HpProgressBar->SetPercent(NewCurrentHp / MaxHp);
+		HpProgressBar->SetPercent(CurrentHp / MaxHp);
 	}
+
+	// HP 스탯 텍스트 업데이트.
+	if (HpStat)
+	{
+		HpStat->SetText(FText::FromString(GetHpStatText()));
+	}
+}
+
+void UABHpBarWidget::UpdateStat(const FABCharacterStat& BaseStat, const FABCharacterStat& ModifierStat)
+{
+	// 최고 HP 값 업데이트.
+	MaxHp = (BaseStat + ModifierStat).MaxHp;
+
+	// 프로그레스 바가 유효하면,
+	if (HpProgressBar)
+	{
+		HpProgressBar->SetPercent(CurrentHp / MaxHp);
+	}
+
+	// 텍스트 블록이 유효하면,
+	if (HpStat)
+	{
+		HpStat->SetText(FText::FromString(GetHpStatText()));
+	}
+}
+
+FString UABHpBarWidget::GetHpStatText()
+{
+	return FString::Printf(TEXT("%.0f / %.0f"), CurrentHp, MaxHp);
 }
 
 void UABHpBarWidget::NativeConstruct()
@@ -32,6 +66,9 @@ void UABHpBarWidget::NativeConstruct()
 	HpProgressBar = Cast<UProgressBar>(GetWidgetFromName(TEXT("PbHpBar")));
 	ensure(HpProgressBar);
 
+	HpStat = Cast<UTextBlock>(GetWidgetFromName(TEXT("TxtHpStat")));
+	ensure(HpStat);
+	
 	// 하고 싶은 것: 캐릭터에 내 정보(위젯)를 전달.
 	// 강참조를 피하기 위해 인터페이스를 통해 우회해 전달 (느슨한 결합).
 	IABCharacterWidgetInterface* CharacterWidget = Cast<IABCharacterWidgetInterface>(OwingActor);
